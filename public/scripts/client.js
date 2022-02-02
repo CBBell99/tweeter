dbe906552248d37199c38358b504db54f19baff2
Chris Bell <chrisbeckertonbell@gmail.com>
2022-02-02 14:47:34 -0800

    add post and get

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,29 +5,29 @@
 */
 
 $(document).ready(function(){
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  },
-      {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-];
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//         "handle": "@SirIsaac"
+//       },
+//     "content": {
+//         "text": "If I have seen further it is by standing on the shoulders of giants"
+//       },
+//     "created_at": 1461116232227
+//   },
+//       {
+//       "user": {
+//         "name": "Descartes",
+//         "avatars": "https://i.imgur.com/nlhLi3I.png",
+//         "handle": "@rd" },
+//       "content": {
+//         "text": "Je pense , donc je suis"
+//       },
+//       "created_at": 1461113959088
+//     }
+// ];
 
 const createTweetElement = function (data){
   const timeAgo = timeago.format(data.created_at) 
@@ -63,20 +63,33 @@ const renderTweets = function (data){
     }
   };
     
-  renderTweets(data);
-
+  
+  const loadTweets = function(){
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      dataType: 'JSON' 
+    }).then(function(data){
+      renderTweets(data)
+    })
+  }
+  loadTweets();
+  
   $('#incoming-tweet').submit(function(event){
     event.preventDefault();
     console.log("form had been submitted")
-    console.log(this)
     const data = $('#incoming-tweet').serialize();
-
     $.ajax({
-      method: 'POST',
       url: '/tweets',
+      method: 'POST',
       data: data
     })
   });
+
+ 
+// renderTweets(data);
+
+
 })
 
 
