d290d9c7ff21f67a5fab1aacb4547586beb48aa6
Chris Bell <chrisbeckertonbell@gmail.com>
2022-02-03 20:37:40 -0800

    add scroll animation on click red arrow

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,40 @@
 /*
-* Client-side JS logic goes here
-* jQuery is already loaded
-* Reminder: Use (and do all your DOM work in) jQuery's document ready function
-*/
+ * Client-side JS logic goes here
+ * jQuery is already loaded
+ * Reminder: Use (and do all your DOM work in) jQuery's document ready function
+ */
 
-$(document).ready(function(){
-
+$(document).ready(function () {
   // escape function
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
+  $("#scroll-button").click(function () {
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $("#tweet-text").offset().top,
+      },
+      0
+    );
+  });
 
-const createTweetElement = function (data){
-  const timeAgo = timeago.format
-  let $tweet =  (`
+  const createTweetElement = function (data) {
+    const timeAgo = timeago.format;
+    let $tweet = `
     <article class="tweets">
       <header>
-        <div>
-          <img src="${escape(data.user.avatars)}">
-          <span>"${data.user.name}"</span>
-        </div>
-          <span id="userhandle">${escape(data.user.handle)}</span>
+      <div>
+        <img src="${escape(data.user.avatars)}">
+        <span>"${data.user.name}"</span>
+      </div>
+        <span id="userhandle">${escape(data.user.handle)}</span>
       </header>
-        <div class="tweet-text">
-          <p>${escape(data.content.text)}</p>
-        </div>
+      <div class="tweet-text">
+        <p>${escape(data.content.text)}</p>
+      </div>
       <footer class="footer-info">
         <p class="time-ago">${timeAgo(data.created_at)}</p>
         <div class="icons">
@@ -37,75 +44,64 @@ const createTweetElement = function (data){
         </div>
       </footer>
     </article>  
-  `);
-  return $tweet;
-};
+    `;
+    return $tweet;
+  };
 
-const renderTweets = function (data){
-  $('.tweets-container').empty();
-  for(let tweet of data){
-    let oldTweet = createTweetElement(tweet);
-     $('.tweets-container').prepend(oldTweet);
+  const renderTweets = function (data) {
+    $(".tweets-container").empty();
+    for (let tweet of data) {
+      let oldTweet = createTweetElement(tweet);
+      $(".tweets-container").prepend(oldTweet);
     }
   };
-  
-  const loadTweets = function(){
+
+  const loadTweets = function () {
     $.ajax({
       url: "/tweets",
       method: "GET",
-      dataType: 'JSON' 
-    }).then(function(data){
-      
+      dataType: "JSON",
+    }).then(function (data) {
       renderTweets(data);
-    })
+    });
   };
 
   loadTweets();
-  
-  $('#incoming-tweet').submit(function(event){
-    event.preventDefault();
-    console.log("form had been submitted")
-    const data = $('#incoming-tweet').serialize();
-    let tweetLength = $('#tweet-text').val().length
-    
-    $('.error').slideUp().text('')
 
-    if(tweetLength === 0){ 
-      return $('.error').html('<i class="fa-solid fa-triangle-exclamation"></i>  You cannot post an empty tweet').slideDown(300);
+  $("#incoming-tweet").submit(function (event) {
+    event.preventDefault();
+    console.log("form had been submitted");
+    const data = $("#incoming-tweet").serialize();
+    let tweetLength = $("#tweet-text").val().length;
+
+    $(".error").slideUp().text("");
+
+    if (tweetLength === 0) {
+      return $(".error")
+        .html(
+          '<i class="fa-solid fa-triangle-exclamation"></i>  You cannot post an empty tweet'
+        )
+        .slideDown(300);
     }
-    if(tweetLength > 140){ 
-      return $('.error').html('<i class="fa-solid fa-triangle-exclamation"></i>You cannot post over 140 characters').slideDown(300);
+    if (tweetLength > 140) {
+      return $(".error")
+        .html(
+          '<i class="fa-solid fa-triangle-exclamation"></i>You cannot post over 140 characters'
+        )
+        .slideDown(300);
     }
-    $('form').trigger('reset');
+    $("form").trigger("reset");
 
     $.ajax({
-      url: '/tweets',
-      method: 'POST',
-      data: data
+      url: "/tweets",
+      method: "POST",
+      data: data,
     })
-      .then(() =>{
-      $('.counter').html('140');  
+      .then(() => {
+        $(".counter").html("140");
       })
-      .then(()=>{
+      .then(() => {
         loadTweets();
-
-      })
-    })
-
-  
-
+      });
   });
-
- 
-// renderTweets(data);
-
-
-
-
-// const $tweet = createTweetElement(tweetData);
-
-// $(document).ready(function(){
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-// })
+});
